814dfa5227d5ac3155128fa2ec1ec31724662bef
Маша <114254835+probina192@users.noreply.github.com>
2022-10-07 21:55:22 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,13 +1,9 @@
 # допустим, в среднем в году 365 дней
-uslovie = 8*60/5 #исходя из условия это кол-во экспонатов, которые возможно посмотреть за 1 день
-kolvoExponatov,kolvoVremeni = int(input('Количество экспонатов = ')), int(input('Количество лет = ' ))
-if kolvoExponatov < 0:
-    print('Ошибка, введено неверное значение экспонатов')
-elif kolvoVremeni < 0:
-    print('Ошибка, введено неверное значение времени')
-elif kolvoVremeni==kolvoExponatov==0:
-    print('Ошибка, введены неверные значение экспонатов и времени')
-if kolvoExponatov > 0 & kolvoVremeni >=0:
-    print('Понадобится дней чтобы осмотреть все экспонаты) = ',kolvoExponatov / uslovie)
-if kolvoVremeni > 0 & kolvoExponatov>=0:
-    print('Кол-во экспонатов, осмотренных по заданому времени равно = ',kolvoVremeni*365*24*uslovie )
+uslovie = 8*60/5 #исходя из условия столько человек в среднем может посмотреть экспонатов за 1 день
+kolvoExp,kolvoVremeni = int(input('Количество экспонатов = ')), int(input('Количество лет = ' ))
+if kolvoVremeni | kolvoExp <=0: #проверка если, будут введены данные меньше нуля, что невоможно
+     print('Ошибка, введены неверные значения')
+if kolvoExp > 0 & kolvoVremeni >=0:
+    print('Понадобится времени что осмотреть все экспонаты в днях = ',kolvoExp / uslovie)
+if kolvoVremeni > 0 & kolvoExp>=0:
+    print('Кол-во экспонатов, осмотренных по заданому времени равно = ',kolvoVremeni*365*uslovie )
